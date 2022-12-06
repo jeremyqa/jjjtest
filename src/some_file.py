95469c7839e20595e3b091e7ac2d7506b026325f
Jeremy B <jere011+github@gmail.com>
2022-12-06 16:29:30 -0500

    add a fate dice roller (#1)
    * add a fate dice roller
    * add github action for tests

--- src/some_file.py
+++ src/some_file.py
@@ -1,6 +1,19 @@
 import requests
+from random import randint
 
 
 def make_a_request() -> int:
     response = requests.get('https://example.com')
     return response.status_code
+
+
+def roll_fate(bonus: int = 0) -> int:
+    return sum([_fate_to_number(randint(1, 6)) for _ in range(4)]) + bonus
+
+
+def _fate_to_number(raw: int) -> int:
+    if raw < 3:
+        return -1
+    if raw > 4:
+        return 1
+    return 0
--- src/test_some_file.py
+++ src/test_some_file.py
@@ -1,6 +1,20 @@
 from http import HTTPStatus
-from some_file import make_a_request
+from unittest.mock import patch
+
+from some_file import make_a_request, roll_fate
 
 
 def test_make_a_request() -> None:
     assert make_a_request() == HTTPStatus.OK
+
+
+def test_roll_fate_no_mock() -> None:
+    assert -4 <= roll_fate(0) <= 4
+
+
+class TestRollFate:
+    def test_all_1s_returns_negative_four(self) -> None:
+        with patch('some_file.randint', return_value=1):
+            assert roll_fate(0) == -4
+            assert roll_fate(0) == -4
+            assert roll_fate(0) == -4
